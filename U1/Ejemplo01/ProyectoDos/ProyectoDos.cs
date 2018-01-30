152c7845bfc0352de2eba3fc44bd5c1ce0969912
geceba <gerardoce23@gmail.com>
2018-01-30 14:12:53 -0600

    Cambios de la tercera clase (reverted from commit 148d0afaf40bfcd692e0e3a8b4e4a122b30571b0)

--- U1/Ejemplo01/ProyectoDos/ProyectoDos.cs
+++ U1/Ejemplo01/ProyectoDos/ProyectoDos.cs
@@ -12,14 +12,13 @@ namespace ProyectoDos
 {
     public partial class ProyectoDos : Form
     {
-        Int16 a, b, c, cuantos, contador=0;
+        Int16 a, b, c;
         decimal d;
         Libreria.Libreria o = new Libreria.Libreria();
 
         public ProyectoDos()
         {
             InitializeComponent();
-            TxtBoxCuantos.Enabled = true;
         }
 
         private void btnCalc_Click(object sender, EventArgs e)
@@ -27,53 +26,21 @@ namespace ProyectoDos
             //d = (a + b + c) / 3;
             d = o.calPromedio(a, b, c);
             lblResultado.Text = Convert.ToString(d);
-
-            contador++;
-
-            if(contador == cuantos)
-            {
-                lblMensaje.Text = Convert.ToString("Ya son N veces ...");
-            }
-            else
-            {
-                //Limpiar campos
-                TxBDatoA.Text = "";
-                TxBDatoB.Text = "";
-                TxBDatoC.Text = "";
-            }
         }
 
         private void TxBDatoA_TextChanged(object sender, EventArgs e)
         {
-            TxtBoxCuantos.Enabled = false;
-            if (TxBDatoA.Text!="")
-            {
-                a = Convert.ToInt16(TxBDatoA.Text);
-            }
-            
-        }
-
-        private void TxtBoxCuantos_TextChanged(object sender, EventArgs e)
-        {
-            cuantos = Convert.ToInt16(TxtBoxCuantos.Text);
+            a = Convert.ToInt16(TxBDatoA.Text);
         }
 
         private void TxBDatoB_TextChanged(object sender, EventArgs e)
         {
-            if (TxBDatoB.Text != "")
-            {
-                b = Convert.ToInt16(TxBDatoB.Text);
-            }
-           
+            b = Convert.ToInt16(TxBDatoB.Text);
         }
 
         private void TxBDatoC_TextChanged(object sender, EventArgs e)
         {
-            if (TxBDatoC.Text != "")
-            {
-                c = Convert.ToInt16(TxBDatoC.Text);
-            }
-            
+            c = Convert.ToInt16(TxBDatoC.Text);
         }
     }
 }
